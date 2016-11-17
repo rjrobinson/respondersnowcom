4fe579289f84c1ce263a68dd890838be02fc64dd
RJ Robinson <Robert_Robinson3@cable.comcast.com>
2016-11-16 21:53:39 -0500

    [1-set-up-responders][ RJ ] updated gem file

--- spec/models/responder_spec.rb
+++ spec/models/responder_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
-describe Responderdo
-  
+describe Responder do
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+  end
+
 end
