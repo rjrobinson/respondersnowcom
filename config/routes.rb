3e9dd0fe680a3ead8977e7a327718d334345fb0d
RJ Robinson <Robert_Robinson3@cable.comcast.com>
2016-11-16 20:59:57 -0500

    [1-set-up-responders][ RJ ] save

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
   # You can have the root of your site routed with 'root'
   root to: 'dashboards#dashboard_1'
 
