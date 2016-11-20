f7e8d0a7a249e68d3717d048115606ee99bd7711
RJ Robinson <Robert_Robinson3@cable.comcast.com>
2016-11-20 09:14:15 -0500

    [1-set-up-responders][ RJ ] removed tmp folder from git hx

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :agencies
   devise_for :responders
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # You can have the root of your site routed with 'root'
