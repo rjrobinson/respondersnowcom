b3c474c6449c2290cd2078e1a1373f24dd3b16ad
RJ Robinson <robert_robinson3@cable.comcast.com>
2016-11-26 12:09:29 -0500

    [2-add-work-history][ RJ ] added routes for authenticated responders, and directed unauthenticated users to the landing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # You can have the root of your site routed with 'root'
-  root to: 'dashboards#dashboard_1'
+
+
+  authenticated :responder do
+    root to: 'dashboards#dashboard_2'
+  end
+
+  root to: 'landing#index'
+  # get 'responder_root' => 'dashboards#dashboard_2', as: :responder_root
 
   # All routes
   get 'dashboards/dashboard_1'
