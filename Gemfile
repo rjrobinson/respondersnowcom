d1fd4e2f321ce486c542017bc164d3ce36f35122
RJ Robinson <robert_robinson3@cable.comcast.com>
2016-12-11 11:21:23 -0500

    [add-paperclip][ RJ ] added procfile, and prepared for deployment

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+ruby '2.2.5'
 
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 gem 'bootstrap-sass'
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -229,6 +229,9 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  if Rails.env.production?
+    config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  else
+    config.omniauth :facebook, ENV['FACEBOOK_DEV_ID'], ENV['FACEBOOK_DEV_SECRET']
+  end
 end
