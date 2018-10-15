a73f668af3caa39cc464cb1a12428b62724deecc
RJ Robinson <robertj.robinson@gmail.com>
2018-10-14 20:22:17 -0400

    [graphql-add][ RJ ] tests

--- app/graphql/types/location_type.rb
+++ app/graphql/types/location_type.rb
@@ -2,13 +2,13 @@ class Types::LocationType < Types::BaseObject
 
   field :id, ID, null: false
 
-  field :latitude, Float, null: false
-  field :longitude, Float, null: false
+  field :latitude, Float, null: true
+  field :longitude, Float, null: true
 
   # field :address, String, null: false
-  field :street, String, null: false
+  field :street, String, null: true
   field :state, String, null: false
-  field :county, String, null: false
+  field :county, String, null: true
   field :city, String, null: false
   field :box, String, null: true
   field :zipcode, String, null: true
