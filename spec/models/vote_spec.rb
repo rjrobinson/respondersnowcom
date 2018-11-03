275b058d566436a57c61753c354468e8fb14ac5a
RJ Robinson <robertj.robinson@gmail.com>
2018-11-03 13:28:34 -0400

    [issue-28][ RJ ] added votes testing

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,5 +3,60 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+
+    it {should belong_to :user}
+
+    it {should belong_to :voteable}
+
+    it {should validate_presence_of :vote_value}
+
+
+    it 'should validate that the user can only vote once' do
+      incident = create(:incident)
+      user = create(:user)
+      Vote.create(voteable: incident, user: user, vote_value: 1)
+
+      other_vote = Vote.new(voteable: incident, user: user, vote_value: 1)
+      other_vote.valid?
+
+      expect(other_vote.errors.count).to eq 1
+    end
+  end
+
+  describe "#upvote" do
+    it 'should increase the vote by 1' do
+      incident = create(:incident)
+      user = create(:user)
+
+      vote = Vote.create(voteable: incident, user: user)
+      vote.upvote
+
+      expect(vote.vote_value).to be 1
+    end
+  end
+
+  describe "#downvote" do
+    it 'should decrease the vote by 1' do
+      incident = create(:incident)
+      user = create(:user)
+
+      vote = Vote.create(voteable: incident, user: user)
+      vote.downvote
+
+      expect(vote.vote_value).to be -1
+    end
+  end
+
+  describe "#remove" do
+    it 'should decrease the vote by 1' do
+      incident = create(:incident)
+      user = create(:user)
+
+      vote = Vote.create(voteable: incident, user: user)
+      vote.remove
+
+      expect(vote.vote_value).to be 0
+    end
+  end
 end
