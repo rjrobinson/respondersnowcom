ec82145c06b7db04fc4e9e0e414842f3eb0571be
RJ Robinson <robertj.robinson@gmail.com>
2018-11-03 11:03:29 -0400

    [issue-28][ RJ ] added county sub tests

--- spec/models/county_subscription_spec.rb
+++ spec/models/county_subscription_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe CountySubscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it {should belong_to(:user)}
+    it {should belong_to(:county)}
+  end
 end
