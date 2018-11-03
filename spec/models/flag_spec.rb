c898409af0036a1b0b0ecb0aec514db40c28c7a8
RJ Robinson <robertj.robinson@gmail.com>
2018-11-03 11:12:19 -0400

    [issue-28][ RJ ] added test for flags

--- spec/models/flag_spec.rb
+++ spec/models/flag_spec.rb
@@ -3,5 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Flag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+
+    it {should belong_to :user}
+
+    it {should belong_to :flaggable}
+
+    it {should validate_presence_of :reason}
+
+    it 'should validate that the user can only flag once' do
+      incident = create(:incident)
+      user = create(:user)
+      Flag.create(flaggable: incident, user: user, reason: "no reason")
+
+      other_flag = Flag.new(flaggable: incident, user: user, reason: "no reason")
+      other_flag.valid?
+
+      expect(other_flag.errors.count).to eq 1
+
+    end
+  end
+
+
 end
