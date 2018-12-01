9b087684ec72fe35c8dd4f2eebf8722e6da4afac
RJ Robinson <robert_robinson3@comcast.cable.com>
2018-12-01 08:58:45 -0500

    [issue-42][ RJ ] added basic map.

--- app/javascript/packs/live.jsx
+++ app/javascript/packs/live.jsx
@@ -1,40 +1,56 @@
-import ApolloClient from 'apollo-boost';
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom';
-// import { ApolloClient } from 'apollo-client';
-import {HttpLink} from 'apollo-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
 
-// import {Provider} from 'react-redux'
+import GoogleMapReact from "google-map-react";
 
-const RESPONDERSNOW_BASE_URL = '/api/graphql';
+const AnyReactComponent = ({text}) => <div>{text}</div>;
 
-const httpLink = new HttpLink({
-    uri: RESPONDERSNOW_BASE_URL
-});
+class RegionalMap extends Component {
+    static defaultProps = {
+        center: {
+            lat: 40.4671416,
+            lng: -74.4664551
+        },
+        zoom: 15
+    };
 
-const TITLE = 'RespondersNOW Graphql'
-const cache = new InMemoryCache();
-
-const client = new ApolloClient({
-    link: httpLink,
-    cache,
-});
-
-
-
-class LiveMap extends Component {
     render() {
         return (
-            <div>
-                <h3>{TITLE}</h3>
+            <div className="row">
+                <div className="col-lg-4">
+                    <div className="tabs-container">
+                    </div>
+                </div>
+                <div className="col-lg-8">
+                    <div className="tabs-container">
+
+                        {/*// Important! Always set the container height explicitly*/}
+                        <div style={{height: '75vh', width: '100%'}}>
+                            <GoogleMapReact
+                                bootstrapURLKeys={{key: 'AIzaSyB1ZUug1hdyRxGcoGoQoAOVJjnqkWGBQ-I'}}
+                                defaultCenter={this.props.center}
+                                defaultZoom={this.props.zoom}
+                            >
+                                <AnyReactComponent
+                                    lat={40.4671416}
+                                    lng={-74.4664551}
+                                    text={"RJ's House"}
+
+                                />
+                            </GoogleMapReact>
+                        </div>
+                    </div>
+                </div>
             </div>
-        )
+
+        );
     }
 }
 
+export default RegionalMap;
 
-export {LiveMap}
-
-ReactDOM.render(< LiveMap/>, document.querySelector('#live-map'))
+ReactDOM.render(
+    < RegionalMap/>,
+    document.querySelector('#live-map')
+);
 
