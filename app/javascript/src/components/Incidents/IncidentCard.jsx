b4f2a6ba082a0d813293818d95c071246b3d1ce5
RJ Robinson <robert_robinson3@comcast.cable.com>
2018-12-24 22:24:59 -0500

    added feature structure to look at incident updates.

--- app/javascript/src/components/Incidents/IncidentCard.jsx
+++ app/javascript/src/components/Incidents/IncidentCard.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {Component} from 'react'
 import TimeAgo from 'react-timeago'
-import {Button, ButtonGroup, Label} from 'react-bootstrap';
+import {Button, ButtonGroup, Label, Panel, Form,FormGroup, FormControl } from 'react-bootstrap';
 import Moment from 'react-moment';
 import FeedElement from '../UIComponents/FeedElement'
 
@@ -8,7 +8,13 @@ import {ConfirmButton} from './ConfirmButton'
 import {UpVoteButton} from './UpVoteButton'
 import {DownVoteButton} from './DownVoteButton'
 
-const IncidentCard = ({incident}) => {
+class IncidentCard extends React.Component {
+
+    state = {open: false}
+    
+    render() {
+    
+    const {incident} = this.props
 
     const {message, location, status, createdAt, confirms, id, upvotes, downvotes} = incident;
 
@@ -47,12 +53,37 @@ const IncidentCard = ({incident}) => {
 
                     <Button>23 <i className="far fa-images"></i></Button>
                     <Button>2 <i className="fas fa-flag"></i></Button>
+                    <Button onClick={() => this.setState({ open: !this.state.open })}>
+                        <i className="fas fa-comments"></i>
+                    </Button>
+                    <Button><i className="fas fa-volume-mute"></i></Button>
                 </ButtonGroup>
-
+                <br/>
+                    <Panel id="collapsible-panel-example-1" expanded={this.state.open} onToggle={() => alert("toggled")}>
+                        <Panel.Collapse>
+                            <Panel.Body>
+                                <ul>
+                                    <li><b>22:32 12/24</b> This part does not work yet. - J0301 </li>
+                                    <li><b>22:32 12/24</b> Units clearning - J0301 </li>
+                                    <li><b>22:31 12/24</b> Bulk of fire is knocked.  - R0302 </li>
+                                    <li><b>22:30 12/24</b> Command reporting that there is a working fire. - R0302  </li>
+                                </ul>
+                                <div className="pull-right">
+                                    <Form inline>
+                                        <FormGroup controlId="formInlineName">
+                                            <FormControl type="text" placeholder="send update.." />
+                                        </FormGroup>{' '}
+                                        <Button type="submit">send</Button>
+                                    </Form>
+                                </div>
+                            </Panel.Body>
+                        </Panel.Collapse>
+                    </Panel>
             </div>
 
         </FeedElement>
-    )
+        )
+    }
 }
 
 
