10e98e4bbb7a4df62e6ba5bbee81f4345ad4cfff
RJ Robinson <robert_robinson3@comcast.cable.com>
2019-02-16 13:02:09 -0500

    added CF endpoint

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   end
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'dve23avlxi3jl.cloudfront.net'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
