5d52dabfb1465132edd503a10c2e68e1f624e995
RJ Robinson <robertj.robinson@gmail.com>
2019-12-05 21:01:35 -0500

    added a better template for the emails.

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 class AdminMailer < ApplicationMailer
-  default from: "RespondersNOW [SUPPORT] <no-reply@respondersnow.com>"
+  default from: "RespondersNOW [ SUPPORT ] <no-reply@respondersnow.com>"
   default to: "robertj.robinson@gmail.com"
+
+
 
   def new_user(user:)
     @user = user
     mail(
-      subject: "New User: #{user.email}"
+        subject: "New User: #{user.email}"
     )
   end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,25 +5,25 @@ require "mandrill"
 
 class ApplicationMailer < ActionMailer::Base
   default(
-    from: "hello@respomdersnow.com",
-    reply_to: "hello@example.com"
+      from:     "hello@respondersnow.com",
+      reply_to: "hello@example.com"
   )
 
   layout 'mailer'
 
   private
 
-    def send_mail(email, subject, body)
-      mail(to: email, subject: subject, body: body, content_type: "text/html")
+  def send_mail(email, subject, body)
+    mail(to: email, subject: subject, body: body, content_type: "text/html")
+  end
+
+  def mandrill_template(template_name, attributes)
+    mandrill = Mandrill::API.new(ENV["MANDRILL_API_KEY"])
+
+    merge_vars = attributes.map do |key, value|
+      { name: key, content: value }
     end
 
-    def mandrill_template(template_name, attributes)
-      mandrill = Mandrill::API.new(ENV["MANDRILL_API_KEY"])
-
-      merge_vars = attributes.map do |key, value|
-        { name: key, content: value }
-      end
-
-      mandrill.templates.render(template_name, [], merge_vars)["html"]
-    end
+    mandrill.templates.render(template_name, [], merge_vars)["html"]
+  end
 end
